7f6c395f6402653952d235a4352fb58f08143192
Edoardo Vacchi <evacchi@users.noreply.github.com>
2023-06-10 22:47:29 +0200

    Cleaner API

--- main/testdata/tinygo/pingpong.go
+++ main/testdata/tinygo/pingpong.go
@@ -15,11 +15,6 @@ var currOffset uint32 = 0
 
 type Address uint32
 
-type Message struct {
-	sender  Address
-	message string
-}
-
 func main() {
 	println("hello")
 }
@@ -32,7 +27,9 @@ func startup(addr uint32) *[SHARED_BUF_SIZE]uint8 {
 
 //export receive
 func receive(size uint32) {
-	message := read(size)
+	reset()
+	message := Message{}
+	message.decode(buf[0:size])
 	fmt.Printf("Received message from %d: '%s'\n", message.sender, message.message)
 	message.sender.Tell(Message{sender: self, message: fmt.Sprintf("ping from %d", self)})
 }
@@ -41,28 +38,12 @@ func (a Address) Tell(message Message) {
 	outgoing++
 	// buffer header
 	binary.LittleEndian.PutUint32(buf[:4], outgoing)
-
-	// message header: target + total len
-	buff := buf[4:]
-
-	binary.LittleEndian.PutUint32(buff[0:4], uint32(a))
-	mlen := uint32(4 + 4 + len(message.message))
-	binary.LittleEndian.PutUint32(buff[4:8], mlen)
-	// message body
-	binary.LittleEndian.PutUint32(buff[8:12], uint32(message.sender))
-	copy(buff[12:12+uint32(len(message.message))], message.message)
-	
+	outBuf := buf[currOffset:]
+	sz := message.encode(a, outBuf)
+	currOffset += sz
 }
 
-func read(size uint32) Message {
+func reset() {
 	currOffset = 4
 	outgoing = 0
-	buff := buf[0:size]
-	address := Address(binary.LittleEndian.Uint32(buff[:4]))
-	message := string(buff[4 : size-4])
-	m := Message{
-		sender:  address,
-		message: message,
-	}
-	return m
 }
